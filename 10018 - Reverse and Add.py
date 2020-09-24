4855a3dd069e204388baace143d0d9a223cf50d1
Pronob <pronobmozumder.info@gmail.com>
2020-09-24 18:52:46 +0600

    Done

---
+++ 10018 - Reverse and Add.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    m = int(input())
+    count = 0
+
+    while True:
+        str_m = str(m)
+        if str_m == str_m[::-1]:
+            break
+        m += int(str_m[::-1])
+        count += 1
+
+    print(count, m)
---
+++ 10023 - Square root.py
@@ -0,0 +1,4 @@
+n1 =int(input())
+for i in range(n1):
+    n = int(input())
+    print(int(n**0.5))
