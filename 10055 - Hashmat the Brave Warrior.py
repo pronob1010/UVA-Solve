09b779d5cc34c31e4e88564e44d68b56c7356973
Pronob <pronobmozumder.info@gmail.com>
2020-09-25 16:14:59 +0600

    Done

---
+++ 10055 - Hashmat the Brave Warrior.py
@@ -0,0 +1,7 @@
+while True:
+    try:
+        x, y = map(int, input().split())
+    except EOFError:
+        break
+
+    print(abs(x - y))
---
+++ 10056 - What is the Probability.py
@@ -0,0 +1,12 @@
+import math
+
+n = int(input())
+for i in range(n):
+    n, p, k = map(float, input().split())
+
+    if p == 0:
+        print("0.0000")
+    elif k == 1:
+        print("{:.4f}".format(p / (1 - math.pow(1 - p, n))))
+    else:
+        print("{:.4f}".format(p * math.pow(1 - p, k - 1) / (1 - math.pow(1 - p, n))))
