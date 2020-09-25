298fb8b2dbd0646c7f34581bcfb4e2f31e764803
Pronob Mozumder <pronobmozumder.info@gmail.com>
2020-09-25 17:32:16 +0600

    Done

---
+++ 10340 - All in All.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        m, n = map(str, input().split())
+    except EOFError:
+        break
+
+    if len(m) > len(n):
+        print("No")
+    else:
+        k = 0
+        for i in range(len(n)):
+            if m[k] == n[i]:
+                k += 1
+            if k == len(m):
+                break
+
+        if k == len(m):
+            print("Yes")
+        else:
+            print("No")
